801218875617c19f2b82af686db0b9f0395ebddd
Piotr Cianciara <piotr.cianciara@comatec.fi>
2022-03-27 10:37:23 +0200

    Add main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import math
+a = 1
+b = -2
+c = -5
+
+def equation(a,b,c):
+  # obliczanie delty
+  D = b * b - 4 * a * c 
+  print(D)
+  if D > 0:
+    x1 =(-b-math.sqrt(D))/(2*a)
+    x2 =(-b+math.sqrt(D))/(2*a)
+    wyniki = (x1 ,x2)
+    print(wyniki)
+    
+  elif D == 0:
+    x1 = -b / 2*a
+    print(x1)
+  else:
+    print("Brak rozwiązań - Delta ujemna")
+  pass
+
+equation(a,b,c)
